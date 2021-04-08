f605e660b2eb8c9e6a47fd8cec287ee16a0029c0
Matt Hale <mhale@factharbor.com>
2021-04-08 16:28:59 -0400

    installed inquirer

--- index.js
+++ index.js
@@ -3,68 +3,88 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = ['Project Title', 'Description', 'Installation Instructions', 'Usage Information', 'Contribution Guidelines', 'Test Instructions', 'License', 'GitHub Username (Questions)', 'Email Address (Questions)'];
+const questions = ['Project Title', 'Description', 'Installation Instructions', 'Usage Information', 'Contribution Guidelines', 'Test Instructions', 'License', 'GitHub Username', 'Email Address'];
 
 // TODO: Create a function to write README file
-const writeReadMe = data => {
-    const fileName = `${data.name.toLowerCase().split(' ').join('')}.json`;
+const writeReadMe = (answers) =>
+    `# ${answers[0]}
 
-    fs.writeToFile(fileName, JSON.stringify(data, null, '\t'), (err) =>
-    err ? console.log(err) : console.log('Success!')  
-    )};
+    ## Description
+    ${answers[1]}
+
+    ## Installation Instructions
+    ${answers[2]}
+
+    ## Usage Information
+    ${answers[3]}
+
+    ## Contribution Guidelines
+    ${answers[4]}
+
+    ## Test Instructions
+    ${answers[5]}
+
+    ## Licenses
+    ${answers[6]}
+
+    [GitHub Profile](https://github.com/${answers[7]})
+
+    [Email Address](mailto:${answers[8]})`;
+
 
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: questions[0],
-                name: 'title',
-            },
-            {
-                type: 'input',
-                message: questions[1],
-                name: 'description',
-            },
-            {
-                type: 'input',
-                message: questions[2],
-                name: 'installation',
-            },
-            {
-                type: 'input',
-                message: questions[3],
-                name: 'usage',
-            },
-            {
-                type: 'input',
-                message: questions[4],
-                name: 'contribution',
-            },
-            {
-                type: 'input',
-                message: questions[5],
-                name: 'testing',
-            },
-            {
-                type: 'input',
-                message: questions[6],
-                name: 'license',
-            },
-            {
-                type: 'input',
-                message: questions[7],
-                name: 'username',
-            },
-            {
-                type: 'input',
-                message: questions[8],
-                name: 'email',
-            },
-        ])
-        .then(writeReadMe(data));
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: questions[1],
+            name: 'description',
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'installation',
+        },
+        {
+            type: 'input',
+            message: questions[3],
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'testing',
+        },
+        {
+            type: 'input',
+            message: questions[6],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: questions[7],
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: questions[8],
+            name: 'email',
+        },
+    ])
+    .then((data) => fs.writeFile('NEWREADME.md', writeReadMe(data)))
+    .then(() => console.log('README successfully created!'))
+    .catch((err) => console.error(err))
 };
 
 // Function call to initialize app
