d5db2002aad7bb2b9ef6b69c3060aebe55f02b55
Matt Hale <mhale@factharbor.com>
2021-04-09 14:42:53 -0400

    separated license bad and link

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
   "Project Title",
   "Description",
@@ -16,12 +16,12 @@ const questions = [
   "Email Address",
 ];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 const writeReadMe = (answers) => {
   fs.writeFile("NewREADME.md", generateMarkdown(answers), (err) => err ? console.log(error) : console.log('Success!'));
 };
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
   inquirer
     .prompt([
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let text;
   switch (license) {
     case "MIT":
-      text = "https://img.shields.io/badge/License-MIT-yellow.svg";
+      text =
+        "![License Badge](https://img.shields.io/badge/License-MIT-yellow.svg)";
       break;
     case "Apache":
-      text = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+      text =
+        "![License Badge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
       break;
     case "Creative Commons":
-      text = "https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg";
+      text =
+        "![License Badge](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)";
       break;
     default:
       "";
@@ -18,19 +21,20 @@ function renderLicenseBadge(license) {
   return text;
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   let text;
   switch (license) {
     case "MIT":
-      text = "https://opensource.org/licenses/MIT";
+      text = "[MIT](https://opensource.org/licenses/MIT)";
       break;
     case "Apache":
-      text = "https://opensource.org/licenses/Apache-2.0";
+      text = "[Apache](https://opensource.org/licenses/Apache-2.0)";
       break;
     case "Creative Commons":
-      text = "http://creativecommons.org/publicdomain/zero/1.0/";
+      text =
+        "[Creative Commons](http://creativecommons.org/publicdomain/zero/1.0/)";
       break;
     default:
       "";
@@ -38,28 +42,24 @@ function renderLicenseLink(license) {
   return text;
 }
 
-// TODO: Create a function that returns the license section of README
+// Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license != null) {
-    return (
-      "![License](" +
-      renderLicenseBadge(license) +
-      ") [License Link](" +
-      renderLicenseLink(license) +
-      ")"
-    );
+    return "This application uses " + renderLicenseLink(license);
   }
   return "";
 }
 
-// TODO: Create a function to generate markdown for README
+// Create a function to generate markdown for README
 function generateMarkdown(answers) {
   console.log(answers);
   return `# ${answers.title}
 
   ## Description
   ${answers.description}
+
+  ${renderLicenseBadge(answers.license)}
 
   # Table of Contents
   - Installation
@@ -74,7 +74,7 @@ function generateMarkdown(answers) {
   ## Usage
   ${answers.usage}
 
-  ## Licenses
+  ## License
   ${renderLicenseSection(answers.license)}
 
   ## Contributing
